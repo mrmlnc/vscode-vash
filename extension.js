a8738a5f4df47e5e457cbba5c217e891160fbc71
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-09-01 19:52:01 +0300

    Static Intellisense

---
+++ extension.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { languages } = require('vscode');
+
+const CompletionProvider = require('./lib/completionProvider');
+
+function activate(context) {
+  const completionItemProvider = new CompletionProvider();
+
+  const completionProviderDisposable = languages
+    .registerCompletionItemProvider('vash', completionItemProvider, '.');
+
+  context.subscriptions.push(completionProviderDisposable);
+}
+
+exports.activate = activate;
